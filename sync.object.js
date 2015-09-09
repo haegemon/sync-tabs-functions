e54b72f3d908edc15100e42fa024899b16dbcb82
pawel <pawel.samysev@gmail.com>
2015-09-10 00:15:18 +0300

    version 0.1

--- sync.object.js
+++ sync.object.js
@@ -9,48 +9,105 @@
  * `localStorage` are always synchronize between tabs. We save at local storage data that need every tabs understand if it need to generate request or it need to load local data.  For that we need create unique identity for every tabs. Save at localStorage data about first open tab and every next tab should read data from localStorage instead of server. If tab is closed next open tab start generates request for server and other tab load data from server and etc.
  */
 
-
-
+// todo add check is localstorage available if not run current action
 var SynchObject = {
 
     /**
-     * Is current tab active now
+     * Unique identity of current page
      */
-    isActiveTab: false,
+    uid: false,
 
     /**
-     * Get unique identity of current tab
+     * Time between two tick
      */
-    getUniqIdentity: function () {
-        ;
+    delayBetweenTick: 1000,
+
+    /**
+     * List of jobs that already started
+     */
+    activeJobs: [],
+
+    /**
+     * Get unique identity of current action and tab
+     * @param jobName Name of job, for multiple function at synchronization
+     */
+    getUniqIdentity: function (jobName) {
+        return jobName + '_' + this.uid;
     },
 
     /**
      * Get data of active tab from storage
+     * @param jobName Name of job, for multiple function at synchronization
      */
-    getActiveTabData: function () {
-    },
-
-    /**
-     * Check is active tab is alive
-     */
-    checkIsActiveTabAlive: function () {
-
+    isTabActive: function (jobName) {
+        var activeTab = JSON.parse(localStorage.getItem("syncObject_" + jobName));
+        return (activeTab.activeTab == this.getUniqIdentity(jobName));
     },
 
     /**
      * Act function. If current tab not active return false and set isActiveTab to false
-     * @param funct Function that should be run
-     * @param jobName Name of job, for multiple function at synchronization
+     * @param obj With 3 params of current work
+     * obj.jobName (mandatory) - Name of current job
+     * obj.activeFunction (mandatory) - Function that should be call if it`s active tab hob
+     * obj.passiveFunction - Function that should be call if it isn`t active job
      */
-    act: function (funct, jobName) {
+    act: function (obj) {
+        this.init(obj.jobName);
+        if (this.isTabActive(obj.jobName)) {
+            console.log('Act', obj.jobName, this.uid);
+            if (obj.activeFunction instanceof 'function') {
+                obj.activeFunction();
+            }
+        } else {
+            if (obj.passiveFunction instanceof 'function') {
+                obj.passiveFunction();
+            }
+        }
 
+        this.timeLastTick = this.timeCurrentTick;
     },
 
     /**
      * Set current tab as active tabs
+     * @param jobName Name of job, for multiple function at synchronization
      */
-    setCurrentTabActive: function () {
-    }
+    setCurrentTabActive: function (jobName) {
+        localStorage.setItem("syncObject_" + jobName, JSON.stringify({
+            lastTick: Date.now(),
+            activeTab: this.getUniqIdentity(jobName)
+        }));
+    },
 
+    /**
+     *
+     * @param jobName
+     */
+    tik: function (jobName) {
+        var activeTab = JSON.parse(localStorage.getItem("syncObject_" + jobName));
+        if (!activeTab
+            || (activeTab.lastTick < (Date.now() - 2 * this.delayBetweenTick))
+            || this.isTabActive(jobName)) {
+            this.setCurrentTabActive(jobName);
+        }
+        var self = this;
+        setTimeout(
+            function () {
+                self.tik(jobName)
+            },
+            this.delayBetweenTick
+        );
+    },
+
+    /**
+     * Initialization of object for special work
+     */
+    init: function (jobName) {
+        if (!this.uid) {
+            this.uid = Math.random();
+        }
+        if (this.activeJobs.indexOf(jobName) === -1) {
+            this.activeJobs.push(jobName);
+            this.tik(jobName);
+        }
+    }
 };
